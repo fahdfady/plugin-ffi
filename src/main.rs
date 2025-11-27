c25b35a4968473be9e8133128d451a42b470d8d4
Fahd Ashour <fahd.fady212@gmail.com>
2025-11-27 19:31:55 +0200

    find shared lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
+use std::fs;
+
+use libloading::Library;
+
+struct PluginInfo {
+    name: *const i8,
+    author: *const i8,
+    version: i32,
+}
+
+struct Plugin {
+    lib: Library,
+    info: PluginInfo,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    for entry in fs::read_dir("./plugins")? {
+        let path = entry?.path();
+        let ext = path.extension().unwrap();
+        if ext == "so" {
+            println!("found lib: {}", path.display());
+        }
+    }
+
     println!("hello world");
+
+    Ok(())
 }
